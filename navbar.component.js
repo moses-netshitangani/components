45de02c20290cb5b7842cb6a0295d2ebe4a0a570
Moses <netshitanganimoses@gmail.com>
2020-05-19 22:22:12 +0200

    added a burger menu

--- navbar.component.js
+++ navbar.component.js
@@ -2,16 +2,17 @@ import React from 'react';
 import '../App.css';
 
 function Navbar(){
+    // let menu_pressed = false;
+
     return(
         <nav>
             <div className="logo">
-                Vasco Media logo would go here
             </div>
-            <ul className="nav-items">
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+            <div className="burger">
+                <div className="burger-bar"></div>
+                <div className="burger-bar"></div>
+                <div className="burger-bar"></div>
+            </div>
         </nav>
     )
 }

9ddcd5b2c534188fda6b2361c4dc758e0bb5fcd4
Moses <netshitanganimoses@gmail.com>
2020-05-20 23:31:30 +0200

    a full article template component

---
+++ full-article.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function FullArticle(){
+
+    return(
+        <div>
+           Hey I'm a template. How do you feel about that?
+        </div>
+    )
+}

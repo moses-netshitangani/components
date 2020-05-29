091e7053fb9d91a1abd68c6cf78ac48e73770ce8
Moses <netshitanganimoses@gmail.com>
2020-05-29 15:02:54 +0200

    burger menu added

--- navbar.component.js
+++ navbar.component.js
@@ -3,14 +3,22 @@ import '../App.css';
 
 function Navbar(){
     const [menu, setMenu] = useState(false);
-
     const updateMenu = () => {
         setMenu(!menu);
-        if(menu){
-
-        }
     }
     
+    let style_menu = {
+        display: menu ? 'flex' : 'none',
+        borderRadius: '5px',
+        background: 'linear-gradient(to right, #cf2f50, #f27242)',
+        flexDirection: 'column',
+        width: '60%',
+        // height: '200px',
+        position: 'absolute',
+        right: '0',
+        zIndex: '1'
+    }
+
     return(
         <nav>
             <div className="logo">
@@ -20,7 +28,7 @@ function Navbar(){
                 <div className="burger-bar"></div>
                 <div className="burger-bar"></div>
             </a>
-            <div className='openMenu'>
+            <div className='openMenu' style={style_menu}>
                 <a href=""><h4>Home</h4></a>
                 <a href=""><h4>About</h4></a>
                 <a href=""><h4>Contact</h4></a>

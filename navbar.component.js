5a1da9bbc230c017938084ee2284dee88bb59bde
Moses <netshitanganimoses@gmail.com>
2020-06-03 23:09:25 +0200

    menu window now slides up and down when burger menu clicked

--- navbar.component.js
+++ navbar.component.js
@@ -11,32 +11,24 @@ const Navbar = props => {
     
     const updateBurgerBar = menu => {
         if (menu) {
-            console.log("entered updateBurgerBar func")
             setClasses("burger-bar x");
-            // setMenu(!menu);
         }
         else {
-            console.log("exited updateBurgerBar func")
             setClasses("burger-bar bars");
-            // setMenu(!menu);
         }
     }
 
     const updateSlide = menu => {
         if(menu){
-            console.log("entered updateSlide func")
             setSlideClasses("drawer long");
         }
         else{
-            console.log("exited updateSlide func")
             setSlideClasses("drawer short");
         }
     }
 
     const updateMenu = () => {
-        console.log("clicked burger-button with menu value of: "+menu);
         setMenu(menu = !menu);
-        console.log("value of menu is now: "+menu);
         updateBurgerBar(menu);
         updateSlide(menu);
     }

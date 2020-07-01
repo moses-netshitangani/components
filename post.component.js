4b768e4949fb202a3ac840b291649bc673bfabb0
Moses <netshitanganimoses@gmail.com>
2020-07-01 20:02:41 +0200

    added form controls

--- post.component.js
+++ post.component.js
@@ -7,8 +7,16 @@ function Post(){
         <div style={{minHeight: '100vh', backgroundColor: 'teal', marginTop: '10em'}}>
             <form>
                 {/* author name & date of post */}
-                <input type='text' name='author' id='author'/>
-                <input type='text' name='date' id='date'/>
+                <div className='post-details'>
+                    <label>
+                        Author:
+                        <input type='text' name='author' id='author' />
+                    </label>
+                    <label>
+                        Date(Year Month Day):
+                        <input type='text' name='date' id='date' />
+                    </label>
+                </div>
 
                 {/* post title, image & and the post content */}
                 <input type='text' name='title' id='title'/>

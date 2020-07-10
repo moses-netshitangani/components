8d89d7d482a56fe69b3a7d318025cf66f8980ea6
Moses <netshitanganimoses@gmail.com>
2020-07-10 16:27:46 +0200

    now has access to id of post clicked

--- full-article.js
+++ full-article.js
@@ -1,12 +1,29 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
+import {BlogContext} from './blog-context';
 import './article.css';
 
 function FullArticle(){
+    // acquiring data from BlogContext component
+    const data = useContext(BlogContext);
+    let [post, changePost] = useState([]);
 
-    // Some components scroll to the middle of the page
     useEffect(() => {
-        window.scrollTo({top: 0, behavior: 'smooth'});
-    });
+        // Some components scroll to the middle of the page
+        window.scrollTo(0, 0);
+
+        // Obtaining article id for display
+        let id = window.location.href;
+        id = id.substring(id.lastIndexOf("/")+1);
+
+        // searching data for article matching id
+        let match = data.filter(article => {
+            return article._id === id;
+        });
+        changePost(match);
+        
+    }, []);
+
+    console.log(post.title);
     
     // styling
     const style_outer = {
@@ -25,7 +42,7 @@ function FullArticle(){
 
     return(
         <div style={style_outer}>
-            <h1 style={style_heading}>This is the article heading.</h1>
+            <h1 style={style_heading}>hey</h1>
             <div className="article-img">
                 This is where the article image will go.
             </div>

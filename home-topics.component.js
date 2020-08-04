2d2857fd0c0f36cc3e5825a22277af95ea943751
Moses <netshitanganimoses@gmail.com>
2020-08-04 22:37:53 +0200

    reverted to old styling

--- home-topics.component.js
+++ home-topics.component.js
@@ -7,12 +7,12 @@ function Topics(){
     const style_parent = {
         // height: '100%',
         // width: '100%',
-        // margin: '5.75% 3.5%',
+        margin: '5.75% 3.5%',
         display: 'flex',
         justifyContent: 'space-between',
-        // textAlign: 'center',
+        textAlign: 'center',
         borderBottom: '1.5px solid black',
-        backgroundColor: 'lime',
+        backgroundColor: 'white',
         position: 'relative',
         top: '0'
     }
@@ -29,36 +29,25 @@ function Topics(){
     const style_outer = {
         width: '100%',
         height: '8.5%',
-        backgroundColor: 'purple',
+        backgroundColor: 'white',
         position: 'fixed',
         top: '10%',
         borderTopLeftRadius: '1em',
         borderTopRightRadius: '1em'
     }
 
-    // for new div
-    const new_style = {
-        backgroundColor: 'blue',
-        paddingTop: '1%',
-        height: '80%',
-        position: 'relative',
-        bottom: '0'
-    }
-
     return(
         <div style={style_outer}>
-            <div style={new_style}>
-                <div style={style_parent}>
-                    <Link to='/topic/News' style={{textDecoration: 'none'}}>
-                        <h4 style={style_child}>News</h4>
-                    </Link>
-                    <Link to='/topic/Lifestyle' style={{textDecoration: 'none'}}>
-                        <h4 style={style_child}>Lifestyle</h4>
-                    </Link>
-                    <Link to='/topic/Fashion' style={{textDecoration: 'none'}}>
-                        <h4 style={style_child}>Fashion</h4>   
-                    </Link>
-                </div>
+            <div style={style_parent}>
+                <Link to='/topic/News' style={{textDecoration: 'none'}}>
+                    <h4 style={style_child}>News</h4>
+                </Link>
+                <Link to='/topic/Lifestyle' style={{textDecoration: 'none'}}>
+                    <h4 style={style_child}>Lifestyle</h4>
+                </Link>
+                <Link to='/topic/Fashion' style={{textDecoration: 'none'}}>
+                    <h4 style={style_child}>Fashion</h4>   
+                </Link>
             </div>
         </div>
     )

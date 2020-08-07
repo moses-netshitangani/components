0ffe549f0ed298d464b66fa7d09674517d74bef4
Moses <netshitanganimoses@gmail.com>
2020-08-07 19:33:41 +0200

    added CKeditor plugin

--- post.component.js
+++ post.component.js
@@ -1,6 +1,7 @@
-import React,  {useEffect} from 'react';
+import React,  {useState, useEffect} from 'react';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import './post.css';
-import { useState } from 'react';
 const axios = require('axios');
 
 function Post(){
@@ -53,6 +54,24 @@ function Post(){
     return(
         <div className='form'>
             <h1>Write a Post</h1>
+            <CKEditor
+                    editor={ ClassicEditor }
+                    data="<p>Hello from CKEditor 5!</p>"
+                    onInit={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
             <form onSubmit={onSubmit}>
                 {/* author name & date of post */}
                 <div className='post-details'>

420a8b006d6f7a93fb8af45292c498849e2a8747
Moses <netshitanganimoses@gmail.com>
2020-12-11 19:02:37 +0200

    mobile footer boilerplate

---
+++ footer.component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+
+    return(
+        <div className="footer-container">
+            <div className="footer-logo img"></div>
+            <div className="social-icons">
+                <div className="insta img"></div>
+                <div className="twitter img"></div>
+                <div className="email img"></div>
+            </div>
+            <div className="copyright">
+                <p>Copyright &copy; 2020 Vasco Media</p>
+                <p>All Rights Reserved</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;

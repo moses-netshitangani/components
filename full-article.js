b796d96cd75c638ec1f2a736aa68ec7039f27eff
Moses <netshitanganimoses@gmail.com>
2020-12-27 17:50:53 +0200

    Disqus comments API integration

--- full-article.js
+++ full-article.js
@@ -5,14 +5,16 @@ import './article.css';
 function FullArticle(){
 
     let [post, changePost] = useState({});
+    let url = "";
+    let id = "";
 
     useEffect(() => {
         // Some components scroll to the middle of the page
         window.scrollTo(0, 0);
 
         // Obtaining article id for display
-        let id = window.location.href;
-        id = id.substring(id.lastIndexOf("/") + 1);
+        url = window.location.href;
+        id = url.substring(url.lastIndexOf("/") + 1);
 
         // fetch specific article using id
         axios.get(`http://localhost:3000/articles/single/${id}`)
@@ -24,6 +26,19 @@ function FullArticle(){
             .catch(err => console.log(err));
         
     }, []);
+
+    let disqus_config = () => {
+        this.page.url = url;
+        this.page.identifier = id;
+    };
+
+    (function () {
+        var d = document, s = d.createElement('script');
+        s.src = 'https://vascomedia.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
+
 
     return(
         <div className="article-outer-container">
@@ -41,6 +56,11 @@ function FullArticle(){
             <div>
                 <p className="full-content">{post.content}</p>
             </div>
+
+            <div id="disqus_thread"></div>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                
+            
         </div>
     )
 }
